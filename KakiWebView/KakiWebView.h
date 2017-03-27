f11aba3eb0125e1b95fcd26dab78268956682e0e
makee <wengyang56@163.com>
2017-03-27 13:32:15 +0800

    Init project

---
+++ KakiWebView/KakiWebView.h
@@ -0,0 +1,19 @@
+//
+//  KakiWebView.h
+//  KakiWebView
+//
+//  Created by MK on 2017/3/27.
+//  Copyright © 2017年 makee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KakiWebView.
+FOUNDATION_EXPORT double KakiWebViewVersionNumber;
+
+//! Project version string for KakiWebView.
+FOUNDATION_EXPORT const unsigned char KakiWebViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KakiWebView/PublicHeader.h>
+
+
